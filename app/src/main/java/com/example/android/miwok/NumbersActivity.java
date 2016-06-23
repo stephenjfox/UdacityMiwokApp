61981dbdbb7e4193ba3157a78c0afc32f2fcf1d8
Stephen Fox <stevethecoder34@gmail.com>
2016-06-23 00:35:06 -0600

    Encapsulation and prep for custom ArrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,15 +1,49 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity
 {
+    private static final String TAG = NumbersActivity.class.getName();
+
+    private ArrayList<String> words = new ArrayList<String>(10) {{
+        add("one");
+        add("two");
+        add("three");
+        add("four");
+        add("five");
+        add("six");
+        add("seven");
+        add("eight");
+        add("nine");
+        add("ten");
+    }};
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        logWords();
+
+        AbsListView _listView = (ListView) findViewById(R.id.rootNumbersView);
+        // TODO: why doesn't android.R.layout.simple_list_item2 work?
+        ArrayAdapter<String> _adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        // TODO: switch that ^^^ back to custom vocab_list_item
+        _listView.setAdapter(_adapter);
+    }
+
+    private void logWords() {
+        for ( int i = 0; i < words.size(); i++ ) {
+            Log.d(TAG, "Word at index " + i + ": " + words.get(i));
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/model/Word.java
@@ -0,0 +1,22 @@
+package com.example.android.miwok.model;
+
+/**
+ * Created by stefox2 on 6/23/16.
+ */
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word( String base, String foreign ) {
+        this.mDefaultTranslation = base;
+        this.mMiwokTranslation = foreign;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
